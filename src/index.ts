5b5f40f4d6d29a8c9e3ddf87ccd2b0bd9101337e
Luca Tamburrano <ltamburrano@gmail.com>
2019-03-06 10:27:57 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from './decorators/create.decorator';
+export * from './decorators/getAll.decorator';
+export * from './decorators/getById.decorator';
+export * from './decorators/service.decorator';
